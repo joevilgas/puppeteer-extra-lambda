5ee35817e8a4eb0d485088436dba784829afd575
joevilgas <joevilgas3@gmail.com>
2025-05-23 15:14:33 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 echo "command - npx tsc"
 npx tsc
 
-echo "command - aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws-account-id>.dkr.ecr.<region>.amazonaws.com"
-aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws-account-id>.dkr.ecr.<region>.amazonaws.com
+echo "command - aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 637423418587.dkr.ecr.us-east-1.amazonaws.com"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 637423418587.dkr.ecr.us-east-1.amazonaws.com
 
 echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
 docker build --platform linux/amd64 --progress=plain -t scraper .
 
-echo "command - docker tag scraper:latest <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest"
-docker tag scraper:latest <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest
+echo "command - docker tag scraper:latest 637423418587.dkr.ecr.us-east-1.amazonaws.com/scraper:latest"
+docker tag scraper:latest 637423418587.dkr.ecr.us-east-1.amazonaws.com/scraper:latest
 
-echo "command - docker push <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest"
-docker push <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest
+echo "command - docker push 637423418587.dkr.ecr.us-east-1.amazonaws.com/scraper:latest"
+docker push 637423418587.dkr.ecr.us-east-1.amazonaws.com/scraper:latest
